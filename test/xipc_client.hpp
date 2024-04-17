af403dfb33057b3fcb9947926f3de4e101a5a57a
anutosh491 <andersonbhat491@gmail.com>
2024-04-17 13:29:58 +0530

    Updating the ipc client based on the new framework

---
+++ test/xipc_client.hpp
@@ -0,0 +1,29 @@
+/***************************************************************************
+* Copyright (c) 2016, Johan Mabille and Sylvain Corlay                     *
+*                                                                          *
+* Distributed under the terms of the BSD 3-Clause License.                 *
+*                                                                          *
+* The full license is in the file LICENSE, distributed with this software. *
+****************************************************************************/
+
+#ifndef XEUS_IPC_CLIENT_HPP
+#define XEUS_IPC_CLIENT_HPP
+
+#include "xeus-zmq/xclient_zmq.hpp"
+
+namespace xeus
+{
+    class xipc_client : public xclient_zmq
+    {
+
+    public:
+
+        // TODO
+
+    private:
+
+        // TODO
+    };
+}
+
+#endif
